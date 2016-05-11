d746a52b8a8482b97b3166b41d28ff4e3c10fcef
Johannes Dorn <johannes@dorn.tk>
2016-05-11 18:30:29 +0200

    creates custom view class

---
+++ app/src/main/java/de/volzo/sensors/AccelView.java
@@ -0,0 +1,30 @@
+package de.volzo.sensors;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Johannes on 11.05.2016.
+ */
+public class AccelView extends View {
+
+    public AccelView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw (Canvas canvas) {
+        // draw graph here.
+    }
+
+    protected void onMeasure (int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(200, 200);
+    }
+
+
+
+
+
+}
